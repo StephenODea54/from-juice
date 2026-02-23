2d4611e1465ffa7f0149b6a29711cc66b8562df6
StephenODea54 <odeastephen1@gmail.com>
2026-02-23 00:12:21 -0500

    configure neon branching

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -1,9 +1,13 @@
 import alchemy from "alchemy";
+import { NeonBranch, NeonProject } from "alchemy/neon";
 import { CloudflareStateStore } from "alchemy/state";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
+/* *
+* Alchemy State, Stage, and App Configuration
+*/
 type StageType = "PERSONAL" | "PULL_REQUEST" | "PRODUCTION";
 
 function getStageType(stage: string): StageType {
@@ -29,7 +33,39 @@ const { stageName, stageType } = resolveStage();
 
 await alchemy("from-juice", {
   stage: stageName,
+  password: process.env.ALCHEMY_PASSWORD,
   stateStore: stageType === "PRODUCTION"
     ? (scope) => new CloudflareStateStore(scope)
     : undefined
 });
+
+/* *
+* Neon Configuration
+*/
+const neonApiKey = alchemy.secret(process.env.NEON_API_KEY);
+
+const neonProject = await NeonProject("from-juice-neon-project", {
+  name: "From Juice",
+  apiKey: neonApiKey,
+});
+
+if (stageType === "PRODUCTION") {
+  console.log("Using production Neon Branch")
+} else {
+  const neonBranch = await NeonBranch(`neon-branch-${stageName}`, {
+    name: `${stageType}/${stageName}`,
+    project: neonProject.id,
+    apiKey: neonApiKey,
+    endpoints: [
+      {
+        type: "read_only"
+      },
+      {
+        type: "read_write"
+      }
+    ],
+  });
+
+  console.log(`${stageType} branch ready`);
+  console.log(`Branch name: ${neonBranch.name}`);
+}
